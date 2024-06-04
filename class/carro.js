48645e3d25aec321e7cf2fd522c01c4776b5b1d5
jessinabrandao <jessinafreitas.brandao@gmail.com>
2024-06-04 14:38:54 -0300

    Atualização de ex

--- class/carro.js
+++ class/carro.js
@@ -1,18 +1,14 @@
-class carro{
-    marca;
-    cor;
-    gastoKm;
-
-    constructor(marca, cor, gastoKm){
-        this.marca = marca
-        this.cor = cor
-        this.gastoKm = gastoKm
+class Carro {
+    constructor(marca, cor, gastoKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoKm = gastoKm;
     }
 
-    calcularGasto(distanciaKm, precoCombustivel){
-        return distanciaKm * this.gastoKm * precoCombustivel
+    calcularGastoDePercurso(distancia, precoCombustivel) {
+        return distancia * this.gastoKm * precoCombustivel;
     }
 }
 
-const ford = new carro("ford", "branco", 1 / 12)
-console.log(ford.calcularGasto(70, 5))
+const uno = new Carro("Fiat", "Branco", 1 / 12);
+console.log(uno.calcularGastoDePercurso(70, 5));

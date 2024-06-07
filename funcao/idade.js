5bbbc125433f113eae02f535d47bb5d61b182adc
jessinabrandao <jessinafreitas.brandao@gmail.com>
2024-06-07 11:02:52 -0300

    Atualização do ex

---
+++ funcao/idade.js
@@ -0,0 +1,10 @@
+const anoNascimento = 2000
+const anoAtual = 2024
+
+function calcularAno(anoNascimento, anoAtual){
+    return anoAtual - anoNascimento
+}
+
+const idadePessoa = calcularAno()
+console.log(idadePessoa)
+
